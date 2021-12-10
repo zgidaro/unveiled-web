50c774fc599c165623f91670897815fa040a4622
Alexander Gidaro <zgidaro@gmail.com>
2021-12-10 09:52:11 -0500

    Initialize project using Create React App

---
+++ src/components/Unveiled/Unveiled.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { UnveiledLogo } from '../UnveiledLogo';
+
+import './Unveiled.css';
+
+export const Unveiled = () => {
+  return (
+    <div className="unveiled">
+      <UnveiledLogo height={128} />
+    </div>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Unveiled } from './components/Unveiled/Unveiled';
+
+import './index.css';
+
+ReactDOM.render(<Unveiled />, document.getElementById('root'));
